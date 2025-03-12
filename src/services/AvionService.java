e7d2fc4b84e5fea19a98325f83baccaeb81203f5
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-12 17:38:02 +0300

    ok

---
+++ src/services/AvionService.java
@@ -0,0 +1,84 @@
+package services;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Avion;
+import utils.DbConnect;
+
+public class AvionService {
+
+    // Ajouter un avion
+    public void ajouterAvion(Avion avion) throws Exception {
+        try (Connection connection = DbConnect.getConnection()) {
+            String query = "INSERT INTO avions (capacite, modele) VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, avion.getCapacite());
+            preparedStatement.setString(2, avion.getModele());
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    // Récupérer tous les avions
+    public List<Avion> getAllAvions() throws Exception {
+        List<Avion> avions = new ArrayList<>();
+        try (Connection connection = DbConnect.getConnection()) {
+            String query = "SELECT * FROM avions";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Avion avion = new Avion();
+                avion.setIdAvion(resultSet.getLong("id_avion"));
+                avion.setCapacite(resultSet.getInt("capacite"));
+                avion.setModele(resultSet.getString("modele"));
+                avions.add(avion);
+            }
+        }
+        return avions;
+    }
+
+    // Récupérer un avion par ID
+    public Avion getAvionById(int id) throws Exception {
+        Avion avion = null;
+        try (Connection connection = DbConnect.getConnection()) {
+            String query = "SELECT * FROM avions WHERE id_avion = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                avion = new Avion();
+                avion.setIdAvion(resultSet.getLong("id_avion"));
+                avion.setCapacite(resultSet.getInt("capacite"));
+                avion.setModele(resultSet.getString("modele"));
+            }
+        }
+        return avion;
+    }
+
+    // Mettre à jour un avion
+    public void updateAvion(Avion avion) throws Exception {
+        try (Connection connection = DbConnect.getConnection()) {
+            String query = "UPDATE avions SET capacite = ?, modele = ? WHERE id_avion = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, avion.getCapacite());
+            preparedStatement.setString(2, avion.getModele());
+            preparedStatement.setLong(3, avion.getIdAvion());
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    // Supprimer un avion
+    public void deleteAvion(int id) throws Exception {
+        try (Connection connection = DbConnect.getConnection()) {
+            String query = "DELETE FROM avions WHERE id_avion = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        }
+    }
+}
