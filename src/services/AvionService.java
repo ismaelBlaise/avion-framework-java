179af0d71ee8a443b5bdeef16049b76c71492360
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-12 17:39:32 +0300

    Update AvionService.java

--- src/services/AvionService.java
+++ src/services/AvionService.java
@@ -11,7 +11,7 @@ import utils.DbConnect;
 
 public class AvionService {
 
-    // Ajouter un avion
+    
     public void ajouterAvion(Avion avion) throws Exception {
         try (Connection connection = DbConnect.getConnection()) {
             String query = "INSERT INTO avions (capacite, modele) VALUES (?, ?)";
@@ -22,7 +22,7 @@ public class AvionService {
         }
     }
 
-    // Récupérer tous les avions
+    
     public List<Avion> getAllAvions() throws Exception {
         List<Avion> avions = new ArrayList<>();
         try (Connection connection = DbConnect.getConnection()) {
