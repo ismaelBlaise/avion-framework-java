277751700521c9c557c9385bfec1d0de078a7ec9
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-19 18:25:55 +0300

    Update ReservationService.java

--- src/services/ReservationService.java
+++ src/services/ReservationService.java
@@ -1,8 +1,46 @@
 package services;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import utils.DbConnect;
 
 public class ReservationService {
-    public void creerReservation(){
-        
+    public int creerReservation(String dateReservation, int idStatut, int idClasse, int idVol) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet generatedKeys = null;
+        int idReservation = -1;
+
+        try {
+            connection = DbConnect.getConnection();
+            String sql = "INSERT INTO reservations (date_reservation, id_statut, id_classe, id_vol) VALUES (?, ?, ?, ?) RETURNING id_reservation";
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, dateReservation);
+            preparedStatement.setInt(2, idStatut);
+            preparedStatement.setInt(3, idClasse);
+            preparedStatement.setInt(4, idVol);
+
+            generatedKeys = preparedStatement.executeQuery();
+
+            if (generatedKeys.next()) {
+                idReservation = generatedKeys.getInt(1);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (generatedKeys != null) generatedKeys.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return idReservation;
     }    
 }
